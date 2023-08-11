bf90782e51080aa9507745b94f6db9b73d765155
sarahellis <sarahellislondon@gmail.com>
2023-08-11 17:46:35 +0100

    create and delete challenge finished

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
+from lib.album import *
 
 
 # Connect to the database
@@ -23,5 +24,8 @@ album_repository = AlbumRepository(connection)
 for album in album_repository.all():
     print(album)
 
-print(album_repository.find(4))
+# print(album_repository.find(4))
+album = Album(13, "Test 1", 1970, 5)
+print(album_repository.create(album))
+print(album_repository.delete(13))
 
